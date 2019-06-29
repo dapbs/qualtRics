29a899e2d40e2f8cba32743be97eb9db89b104e1
danbras <daniel@filmeametro.com>
2019-06-29 15:50:46 -0400

    export-responses updates 25

--- R/utils.R
+++ R/utils.R
@@ -360,6 +360,7 @@ download_qualtrics_export <- function(check_url, verbose = FALSE) {
     httr::GET(paste0(check_url, "/file"), httr::add_headers(headers))
   })
   print(f)
+  print(str(f))
   # If content is test request, then load temp file (this is purely for testing)
   # httptest library didn't work the way it needed and somehow still called the API
   # leading to errors
